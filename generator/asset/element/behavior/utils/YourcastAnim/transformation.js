4729888232779860e0b2076ff362663205dd5893
Golfieri <golfieri.guillaume@gmail.com>
2013-06-27 11:25:16 +0200

    Add simple appearance behavior with the timer. Change function in transformation.

--- generator/asset/element/behavior/utils/YourcastAnim/transformation.js
+++ generator/asset/element/behavior/utils/YourcastAnim/transformation.js
@@ -6,10 +6,10 @@
 /**
  *	Modifie la hauteur d'une zone
  */
-function scaleY(id, value, duration) {
+function scaleY(id, value, duration, transition) {
 
 	enleverAnimation(id);
-    ajouterAnimation(id, duration+"s", 'height', 'ease-out');
+    ajouterAnimation(id, duration+"s", 'height', transition);
     $(id).setStyle({ 'height': value });
 
 }
