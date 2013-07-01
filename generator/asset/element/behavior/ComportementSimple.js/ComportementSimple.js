993579c8d4a7afc54805eb9ac641fa2579cf2e11
Golfieri <golfieri.guillaume@gmail.com>
2013-07-01 09:39:05 +0200

    Ajout d'un comportement par défaut.

---
+++ generator/asset/element/behavior/ComportementSimple.js/ComportementSimple.js
@@ -0,0 +1,37 @@
+/**
+ *  Rights :
+ *
+ *      Copyright (c) 2013 YourCast - I3S/CNRS ADAM/INRIA.
+ *
+ *      All rights reserved. This program and the 
+ *      accompanying materials are made available under the 
+ *      terms of the GNU Public License v3.0 which accompanies 
+ *      this distribution, and is available at
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ *  Informations :
+ *
+ *      La classe comportement alternance réutilise la classe
+ *      comportement et alterne deux styles différends de
+ *      couleur pour la maquette de Clément Ader.
+ *
+ *  Versions :
+ *
+ *      1.0.0 : Création d'une classe fonctionnelle.
+ *
+ *  Contributors :
+ *
+ *      Simon Urli (simon.urli@gmail.com)
+ *      Guillaume Golfieri (golfieri.guillaume@gmail.com)
+ */
+
+// Load le script d'apparition
+loadScript(BEHAVIOUR_PATH + "/utils/YourcastAnim/apparition.js");
+
+// Classe
+var ComportementSimple = Class.create(Comportement, {
+
+
+
+});
+
