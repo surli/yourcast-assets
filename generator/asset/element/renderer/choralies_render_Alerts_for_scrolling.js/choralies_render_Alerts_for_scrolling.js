266da513c87959ae4999611eabcd3d1307fb9cdf
Simon Urli <simon.urli@gmail.com>
2013-07-20 19:37:37 +0200

    Changes in renderers

--- generator/asset/element/renderer/choralies_render_Alerts_for_scrolling.js/choralies_render_Alerts_for_scrolling.js
+++ generator/asset/element/renderer/choralies_render_Alerts_for_scrolling.js/choralies_render_Alerts_for_scrolling.js
@@ -9,16 +9,18 @@ function choralies_render_Alerts_for_scrolling(tableau, zone, timeInfo) {
 	timeInfo = typeof timeInfo !== 'undefined' ? timeInfo : 50;
 	var content = "";
 	var time = timeInfo;
-	for (var indice = 0; indice < tableau.alertes.length; indice++) {
-		var elements = tableau.alertes[indice];
+	if (!(is_empty(tableau))) {
+		for (var indice = 0; indice < tableau.alertes.length; indice++) {
+			var elements = tableau.alertes[indice];
 
-		content += "<span class='alert'>";
-		content += "<span class='alert_Content'> - "+elements.contenu+" - </span>";
-		content += "</span>";	
+			content += "<span class='alert'>";
+			content += "<span class='alert_Content'> - "+elements.contenu+" - </span>";
+			content += "</span>";	
 		
-		$(zone.id).style.display="block";
-	}
+			$(zone.id).style.display="block";
+		}
 
-	var dico = {"content": content, "time" : time};
-	zone.pushInfo(dico);
+		var dico = {"content": content, "time" : time};
+		zone.pushInfo(dico);
+	}
 }
